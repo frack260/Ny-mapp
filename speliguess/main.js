41613567f8aea192d25f36731129f0c3302c9825
frack260 <72459087+frack260@users.noreply.github.com>
2021-08-09 00:34:32 +0200

    kk

--- speliguess/main.js
+++ speliguess/main.js
@@ -1,8 +1,12 @@
 let box = document.querySelector('#box')
+
 let box1 = document.querySelector('#box1')
 let box2 = document.querySelector('#box2')
 let box3 = document.querySelector('#box3')
-
+let box5 = document.querySelector('#box5')
+let box6 = document.querySelector('#box6')
+new2 = 0
+ff = 200
 let button1 = document.querySelector('#button1')
 let buttonupg1 = document.querySelector('#buttonupg1')
 
@@ -10,7 +14,7 @@ let container = document.querySelector('#container')
 box1.onclick = add;
 button1.onclick = new1;
 buttonupg1.onclick = upg1
-
+inc = 1
 
 if (localStorage.b === undefined) {
 b = 1
@@ -27,6 +31,12 @@ kkk = 0
 kkk = localStorage.kkk
 console.log('kkk');
 }
+if (localStorage.fff === undefined) {
+fff = 0
+}else {
+fff = localStorage.fff
+console.log('fff');
+}
 load()
 
 
@@ -39,11 +49,15 @@ console.log(localStorage.score);
 
 if (localStorage.score === undefined ) {
  number = 1
+ score = 1
 console.log('bajs');
 }else {
-JSON.parse(myscore)
+  JSON.parse(myscore);
+
 JSON.parse(b)
 JSON.parse(kkk)
+JSON.parse(fff)
+
 number = myscore
   console.log('lol');
 }
@@ -52,14 +66,19 @@ console.log(number);
 }
 parseInt(number)
 if (number = parseInt(number)) {
-  if (b === 100) {
+  if (b == 100) {
     box2.innerHTML = '';
     var new1 = document.createElement("div")
     container.appendChild(new1)
+new2 = document.createElement('button')
+var new3 = document.createTextNode("uppgrade")
+new2.setAttribute('id', 'upg2')
+new2.appendChild(new3)
 
+container.appendChild(new2)
     new1.classList.add('box4')
         new1.classList.add('boxlol')
-        passive();
+        inc = inc + 4
   }
   save()
   oskar()
@@ -75,9 +94,10 @@ score = number
 localStorage.setItem("score", score)
 localStorage.setItem("b", b)
 localStorage.setItem("kkk", kkk)
-
-
+localStorage.setItem("fff", fff)
+console.log(number);
 setTimeout(save, 250)
+
 }
 function load() {
 
@@ -86,46 +106,100 @@ stock1 = localStorage.getItem("b")
 upg1 = localStorage.getItem("kkk")
 
 
-console.log(myscore);
+
 start()
 }
-
+console.log(number);
 
 
 function new1() {
-  if (number >= 250) {
+  if (number >= ff) {
     box2.innerHTML = '';
     var new1 = document.createElement("div")
     container.appendChild(new1)
 
     new1.classList.add('box4')
         new1.classList.add('boxlol')
-        passive();
+    inc += 4
       b = 100
 number = number - 250
+ new2 = document.createElement('button')
+var new3 = document.createTextNode("uppgrade")
+new2.setAttribute('id', 'upg2')
+new2.appendChild(new3)
+console.log(number);
+
+container.appendChild(new2)
   }
 
 }
+
+
+
+
+
+ff = 200
+f = 0
+oskar4()
+function oskar4() {
+console.log(number);
+
+  ff = 200
+
+if (fff == 0 ) {
+ff =200
+console.log(number);
+}else if(fff == 1){
+  ff = fff*ff*1.15
+}else {
+  ff  = Math.round( (fff*0.75)*ff*1.1)
+}
+
+f = 1*fff
+
+}console.log(number);
+
+
 k = 0
 oskar2()
 function oskar2() {
 
 
-kk = 1000
+kk = 100
 
 if (kkk == 0 ) {
-kk = 1000
+kk = 100
 }else if(kkk == 1){
-  kk = kkk*kk*1.25
+  kk = Math.round(  kkk*kk*1.15)
 }else {
-  kk  = Math.round( (kkk*0.75)*kk*1.25)
+  kk  = Math.round( (kkk*0.5)*kk*1.1)
 }
 
-k = 5*kkk
-box3.innerHTML= '<h2 id="upgrade"> '+ kk +'</h2>'
+k = 3*kkk
+box3.innerHTML= '<h2 id="upgrade" class=" no-highlight"> '+ kk +'</h2>'
 
 }
+lol12()
+function lol12() {
+  inc = inc+ f
+}
+function lol123() {
+  inc = inc + 1
+}
 
+function upg2() {
+if (number >= ff) {
+  console.log(kk);
+fff++
+
+number = number - ff
+f = f + 1
+lol123()
+oskar4()
+}
+
+
+}
 
 
 function upg1() {
@@ -135,7 +209,7 @@ kkk++
 box3.innerHTML= '<h2> '+ kk +'</h2>'
 kk
 number = number - kk
-k = k + 5
+k = k + 3
 oskar2()
 }
 
@@ -144,36 +218,84 @@ oskar2()
 
 
 function oskar() {
-box.innerHTML = '<h2 class="no-highlight"> '+ number +'</h2> '
+box.innerHTML = '<h2 class="no-highlight scoreboard"> '+ number + "$"+'</h2> '
 
 
 console.log(number);
 
 
-
+console.log(ff);
 setTimeout(oskar, 100)
 }
 
 
+
 function oskar1() {
 console.log(kkk);
- number+=1
- setTimeout(oskar1, 750)
+if (inc > 1) {
+  c = inc/inc
+
+
+
+    number += c
+
+}
+cc = 1000/inc
+
+setTimeout(oskar1, cc)
+
+
+
 }
 
-
+console.log(number);
+x = 0
 function add() {
 
-x = 10 + k
+x = 1 + k
 
 number+=x
+xx = x + "$+"
 console.log(x);
+addanimation()
+
 }
 
 
-function passive() {
 
-number+=4
 
-  setTimeout(passive, 750)
+
+
+function addanimation() {
+var tt = document.createElement('p')
+  var t = document.createTextNode(xx)
+  tt.appendChild(t)
+  tt.classList.add('fadeOut')
+box6.appendChild(tt)
+$( ".fadeOut").fadeOut(700);
+
+  console.log(t);
+
+
+
 }
+
+
+
+oskar3()
+
+function oskar3() {
+inc1 = inc-1
+  box5.innerHTML = '<h2 class="no-highlight scoreboard"> '+ inc1 +"$: per second "+'</h2> '
+
+console.log('hellosvavrmos');
+console.log(f)
+
+  setTimeout(oskar3, 500)
+
+
+
+}
+new2.onclick = upg2
+
+var x = 0;
